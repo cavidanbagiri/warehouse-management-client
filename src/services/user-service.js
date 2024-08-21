3b95f4e31917f24d7e982e0f3501ddae6f17fe34
cavidanbagiri <cavidanbagiri@gmail.com>
2024-08-21 21:14:54 +0300

    change user login logout urls

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,8 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
+import { API_URL } from "../http";
+
+
 
 class UserService {
 
@@ -7,7 +10,8 @@ class UserService {
         'users/login',
         async (user_data) => {
             let data = null;
-            await axios.post('http://localhost:3001/api/user/login', user_data)
+            // await axios.post('http://localhost:3001/api/user/login', user_data)
+            await axios.post(API_URL + '/user/login', user_data)
                 .then((response) => {
                     data = response.data;
                 }).catch((err)=>{
@@ -21,7 +25,8 @@ class UserService {
         'user/refresh',
         async () => {
             let data = null;
-            await axios.get('http://localhost:3001/api/user/refresh')
+            // await axios.get('http://localhost:3001/api/user/refresh')
+            await axios.get(API_URL + '/user/refresh')
                 .then((response) => {
                     data = response.data;
                 })
@@ -32,7 +37,8 @@ class UserService {
    static userLogout = createAsyncThunk(
        '/users/logout',
        async ()=>{
-           await axios.post('http://localhost:3001/api/user/logout')
+        //    await axios.post('http://localhost:3001/api/user/logout')
+           await axios.post('https://warehouse-management-server-c8n6.onrender.com/api'+'/user/logout')
            .then((response) => {
                console.log('user logout ',response);
            })
